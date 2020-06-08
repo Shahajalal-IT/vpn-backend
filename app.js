c3ea8cae717b91b6b868f82c845ef8c5d9ff1cf9
tuhin1714@cseku.ac.bd <tuhin1714@cseku.ac.bd>
2020-06-09 00:54:10 +0600

    Initial Setup

---
+++ app.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Configure Mongoose to Connect to MongoDB
+mongoose
+    .connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then((response) => {
+        console.log("MongoDB Connected Successfully.");
+    })
+    .catch((err) => {
+        console.log("Database connection failed." + err);
+    });
+
+// Configure Express
+app.use(express.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+app.use("/uploads", express.static(path.join("uploads")));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); //* will allow from all cross domain
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
+
+app.use(cors());
+
+/* Routes */
+const api = require("./routes/api");
+
+app.use("/api", api);
+
+/* Start The Server */
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ routes/api.js
@@ -0,0 +1,20 @@
+var express = require('express')
+
+let router = express.Router()
+// Admin Routes------------------
+let admin = require('./api/admin.route');
+router.use('/admin', admin);
+
+// superAdmin Routes------------------
+let superAdmin = require('./api/superadmin.route');
+router.use('/super-admin', superAdmin);
+
+// Reseller Routes------------------
+let reseller = require('./api/reseller.route');
+router.use('/reseller', reseller);
+
+// User Routes------------------
+let user = require('./api/user.route');
+router.use('/user', user);
+
+module.exports = router;
