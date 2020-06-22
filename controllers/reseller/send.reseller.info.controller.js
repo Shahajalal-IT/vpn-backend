355aa1d9a1a0d387c5887785ec8df7b287d25c8e
tuhin1714@cseku.ac.bd <tuhin1714@cseku.ac.bd>
2020-06-22 20:16:19 +0600

    site name and site title send with reseller info

--- controllers/reseller/send.reseller.info.controller.js
+++ controllers/reseller/send.reseller.info.controller.js
@@ -3,6 +3,7 @@
  */
 const db = require("../../models");
 const reseller = db.reseller;
+const admin = db.admin;
 const Op = db.Sequelize.Op;
 
 exports.getResellerInfo = (req, res, next) => {
@@ -12,12 +13,16 @@ exports.getResellerInfo = (req, res, next) => {
         where:{id: resellerId}
     })
         .then(result => {
-            console.log(result);
-            res.status(200).json({
-                data: result,
-                msg: "Successfully Read Reseller Data",
-                error:false
+            admin.findByPk(result.admin_id).then(admin_info => {
+                res.status(200).json({
+                    data: result,
+                    site_name: admin_info.site_name,
+                    site_title: admin_info.site_title,
+                    msg: "Successfully Read Reseller Data",
+                    error:false
+                })
             })
+
         }).catch(error => {
         return res.status(400).json({error: true, msg: "Reseller Reading Was Unsuccessful",err: error})
     });
