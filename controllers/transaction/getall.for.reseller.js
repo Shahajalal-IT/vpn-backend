401a2a6b823bb1b29c83716ffba00ad42213ca63
tuhin1714@cseku.ac.bd <tuhin1714@cseku.ac.bd>
2020-06-22 22:35:06 +0600

    all transaction data send for reseller

--- controllers/transaction/getall.for.reseller.js
+++ controllers/transaction/getall.for.reseller.js
@@ -7,7 +7,7 @@ const reseller = db.reseller;
 const Op = db.Sequelize.Op;
 const jwt = require('jsonwebtoken');
 exports.getAllTransaction = (req, res, next) => {
-    const adminId = req.adminData.userId;
+    const resellerId = req.resellerData.userId;
     // const fetchedData = req.body.data;
     // const decodedToken = jwt.verify(
     //     fetchedData,
@@ -24,7 +24,7 @@ exports.getAllTransaction = (req, res, next) => {
     var startDate =decodedToken.startDate === '' ? d:decodedToken.startDate;
     var endDate = decodedToken.endDate === '' ? ed:decodedToken.endDate;
     var where = {
-        given_by: adminId,
+        given_by: resellerId,
         createdAt: {
             [Op.between]: [startDate, endDate]
         }
@@ -37,26 +37,26 @@ exports.getAllTransaction = (req, res, next) => {
                 var finalDocuments = [];
                 documents.forEach(function(obj,i) {
 
-                        reseller.findByPk(obj.given_to).then(result => {
-                            var newObj = {
-                                id:obj.id,
-                                given_to:result.user,
-                                given_to_id:obj.given_to,
-                                previous_balance:obj.previous_balance,
-                                current_balance:obj.current_balance,
-                                transaction_type:result.transaction_type,
-                                notes:obj.notes,
-                                createdAt: obj.createdAt
-                            };
-                            finalDocuments.push(newObj);
-                            if(i === documents.length-1){
-                                res.status(200).json({
-                                    data: finalDocuments,
-                                    msg: "Successfully Read Transaction Data",
-                                    error:false
-                                })
-                            }
-                        })
+                    reseller.findByPk(obj.given_to).then(result => {
+                        var newObj = {
+                            id:obj.id,
+                            given_to:result.user,
+                            given_to_id:obj.given_to,
+                            previous_balance:obj.previous_balance,
+                            current_balance:obj.current_balance,
+                            transaction_type:result.transaction_type,
+                            notes:obj.notes,
+                            createdAt: obj.createdAt
+                        };
+                        finalDocuments.push(newObj);
+                        if(i === documents.length-1){
+                            res.status(200).json({
+                                data: finalDocuments,
+                                msg: "Successfully Read Transaction Data",
+                                error:false
+                            })
+                        }
+                    })
                 });
             }
         )
--- routes/api/transaction.route.js
+++ routes/api/transaction.route.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const adminAuth = require('../../auth/admin.auth');
+const resellerAuth = require('../../auth/reseller.auth');
 
-//Super Admin Create--------------------------
+//Get All Transaction for Admin--------------------------
 const getAllTransaction = require('../../controllers/transaction/getall.controller');
 router.post('/get-all-transaction', adminAuth, getAllTransaction.getAllTransaction);
+
+//Get All Transaction for Reseller--------------------------
+const getAllTransactionReseller = require('../../controllers/transaction/getall.for.reseller');
+router.post('/get-all-transaction-reseller', resellerAuth, getAllTransactionReseller.getAllTransaction);
 
 // Export the Router
 module.exports = router;
