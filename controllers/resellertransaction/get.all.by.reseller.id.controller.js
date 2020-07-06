5a8ccc790ef2bf7b0cd2777331702e9df6c54e69
tuhin1714@cseku.ac.bd <tuhin1714@cseku.ac.bd>
2020-07-07 00:46:53 +0600

    createdat and updatedat to getallresellerid

--- controllers/resellertransaction/get.all.by.reseller.id.controller.js
+++ controllers/resellertransaction/get.all.by.reseller.id.controller.js
@@ -21,56 +21,62 @@ exports.getAllResTransByResellerId = (req, res, next) => {
             admin_id: adminId
         }
     }
+    reseller.findByPk(resellerId).then(reseller => {
+        reseller_transaction.paginate(options)
+            .then(
+                documents => {
+                    if(documents.total === 0){
+                        res.status(200).json({
+                            data: [],
+                            pages: 1,
+                            total: 1,
+                            msg: "Successfully Read User Data",
+                            error: false
+                        })
+                    }
+                    var finalDocuments = [];
+                    var i=0;
+                    documents.docs.forEach(function(obj) {
+                        user.findByPk(obj.user_id).then(result => {
+                            if(result === null){
 
-    reseller_transaction.paginate(options)
-        .then(
-            documents => {
-                if(documents.total === 0){
-                    res.status(200).json({
-                        data: [],
-                        pages: 1,
-                        total: 1,
-                        msg: "Successfully Read User Data",
-                        error: false
-                    })
+                            }else{
+                                var newObj = {
+                                    id:obj.id,
+                                    reseller_id:obj.reseller_id,
+                                    user_id:obj.user_id,
+                                    user:result.pin,
+                                    p_balance:obj.p_balance,
+                                    c_balance:obj.c_balance,
+                                    price:obj.price,
+                                    createdAt:obj.createdAt,
+                                    updatedAt:obj.updatedAt
+                                };
+                                finalDocuments.push(newObj);
+                            }
+
+                            if(i === documents.docs.length-1){
+                                res.status(200).json({
+                                    reseller_name: reseller.user,
+                                    data: finalDocuments,
+                                    pages:documents.pages,
+                                    total:documents.total,
+                                    msg: "Successfully Read User Data",
+                                    error:false
+                                })
+                            }
+                            i++;
+                        })
+
+                    });
                 }
-                var finalDocuments = [];
-                var i=0;
-                documents.docs.forEach(function(obj) {
-                    user.findByPk(obj.user_id).then(result => {
-                        if(result === null){
+            )
 
-                        }else{
-                            var newObj = {
-                                id:obj.id,
-                                reseller_id:obj.reseller_id,
-                                user_id:obj.user_id,
-                                user:result.pin,
-                                p_balance:obj.p_balance,
-                                c_balance:obj.c_balance,
-                                price:obj.price,
-                                createdAt:obj.createdAt,
-                                updatedAt:obj.updatedAt
-                            };
-                            finalDocuments.push(newObj);
-                        }
+    }) .catch(error => {
+        return res.status(400).json({error: true, msg: "User Reading Was Unsuccessful",err: error})
+    })
 
-                        if(i === documents.docs.length-1){
-                            res.status(200).json({
-                                data: finalDocuments,
-                                pages:documents.pages,
-                                total:documents.total,
-                                msg: "Successfully Read User Data",
-                                error:false
-                            })
-                        }
-                        i++;
-                    })
 
-                });
-            }
-        )
-        .catch(error => {
-            return res.status(400).json({error: true, msg: "User Reading Was Unsuccessful",err: error})
-        })
+
+
 };
