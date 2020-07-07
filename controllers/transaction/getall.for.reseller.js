eb7020967984619e877b7d8142a8ad04beb30580
tuhin1714@cseku.ac.bd <tuhin1714@cseku.ac.bd>
2020-07-08 00:02:36 +0600

    get transaction data for reseller also done

--- controllers/transaction/getall.for.reseller.js
+++ controllers/transaction/getall.for.reseller.js
@@ -58,55 +58,61 @@ exports.getAllTransaction = (req, res, next) => {
             }
         }
     }
-
-    transaction.paginate(options)
+    reseller.findAll({attributes: ['id', 'user']},{where:{creator: resellerId }})
         .then(
-            documents => {
+            resellers => {
+                transaction.paginate(options)
+                    .then(
+                        documents => {
 
-                if(documents.length === 0){
-                    res.status(200).json({
-                        data: [],
-                        msg: "No Data Available",
-                        error: false
-                    })
-                }
-                var finalDocuments = [];
-                var i=0;
-                documents.forEach(function(obj) {
+                            if (documents.total === 0) {
+                                res.status(200).json({
+                                    data: [],
+                                    msg: "No Data Available",
+                                    error: false
+                                })
+                            }
+                            var finalDocuments = [];
+                            var i = 0;
+                            documents.docs.forEach(function (obj) {
 
-                    reseller.findByPk(obj.given_to).then(result => {
+                                reseller.findByPk(obj.given_to).then(result => {
 
-                        if(result === null){
+                                    if (result === null) {
 
-                        }else {
-                            var newObj = {
-                                id: obj.id,
-                                given_to: result.user,
-                                given_to_id: obj.given_to,
-                                previous_balance: obj.previous_balance,
-                                current_balance: obj.current_balance,
-                                transaction_type: result.transaction_type,
-                                notes: obj.notes,
-                                createdAt: obj.createdAt
-                            };
-                            finalDocuments.push(newObj);
+                                    } else {
+                                        var newObj = {
+                                            id: obj.id,
+                                            given_to: result.user,
+                                            given_to_id: obj.given_to,
+                                            previous_balance: obj.previous_balance,
+                                            current_balance: obj.current_balance,
+                                            transaction_type: result.transaction_type,
+                                            notes: obj.notes,
+                                            createdAt: obj.createdAt
+                                        };
+                                        finalDocuments.push(newObj);
 
+                                    }
+
+                                    if (i === documents.docs.length - 1) {
+                                        res.status(200).json({
+                                            data: finalDocuments,
+                                            msg: "Successfully Read Transaction Data",
+                                            pages:documents.pages,
+                                            total:documents.total,
+                                            resellers: resellers,
+                                            error: false
+                                        })
+                                    }
+
+                                    i++;
+                                })
+
+                            });
                         }
-
-                        if (i === documents.length - 1) {
-                            res.status(200).json({
-                                data: finalDocuments,
-                                msg: "Successfully Read Transaction Data",
-                                error: false
-                            })
-                        }
-
-                        i++;
-                    })
-
-                });
-            }
-        )
+                    )
+            })
         .catch(error => {
             console.log(error)
             return res.status(400).json({error: true, msg: "Transaction Reading Was Unsuccessful",err: error})
